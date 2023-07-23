7abd930367fc0489314e783ce569809cbf2f1ab2
makambi-elvis <makambielvis002@gmail.com>
2023-07-23 16:38:39 +0300

    started master layout

---
+++ public/layout/master.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="content-type" content="text/html" />
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="United Bible Societies Dashboard">
+    <meta name="keywords"
+        content="admin, dashboard, united bible societies, societies">
+
+    <!--<link rel="shortcut icon" href="img/icons/icon-48x48.png" /> -->
+
+    <link rel="canonical" href="index.html" />
+
+    <title>United Bible Societies </title>
+
+    <link href="css/app.css" rel="stylesheet">
+
+
+</head>
