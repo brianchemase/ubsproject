7eb59268c6f0847310e9e12053573bb69fd039ea
makambi-elvis <makambielvis002@gmail.com>
2023-07-26 21:59:04 +0300

    stewardship contribution in income diversification

--- resources/views/users/financial-sustainability/income-diversification.blade.php
+++ resources/views/users/financial-sustainability/income-diversification.blade.php
@@ -7,24 +7,24 @@
     <div class="app-content content">
         <div class="content-wrapper">
             <div class="content-wrapper-before"></div>
-                <div class="content-header row">
-                    <div class="content-header-left col-md-4 col-12 mb-2">
-                        <h3 class="content-header-title">Income Diversification form</h3>
-                    </div>
-                    <div class="content-header-right col-md-8 col-12">
-                        <div class="breadcrumbs-top float-md-right">
-                            <div class="breadcrumb-wrapper mr-1">
-                                <ol class="breadcrumb">
-                                    <li class="breadcrumb-item"><a href="index.html">Home</a>
-                                    </li>
-                                    <li class="breadcrumb-item active">Form
-                                    </li>
-                                </ol>
-                            </div>
+            <div class="content-header row">
+                <div class="content-header-left col-md-4 col-12 mb-2">
+                    <h3 class="content-header-title">Income Diversification form</h3>
+                </div>
+                <div class="content-header-right col-md-8 col-12">
+                    <div class="breadcrumbs-top float-md-right">
+                        <div class="breadcrumb-wrapper mr-1">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="index.html">Home</a>
+                                </li>
+                                <li class="breadcrumb-item active">Form
+                                </li>
+                            </ol>
                         </div>
                     </div>
                 </div>
             </div>
+        </div>
 
         <div class="content-body">
 
@@ -139,6 +139,7 @@
                         </div>
                     </div>
                 </div>
+
                 <div class="row match-height mx-auto gx-3">
                     <div class="card col-xl-4 col-lg-6 col-12 ">
                         <div class="card-block">
@@ -153,7 +154,24 @@
                             </div>
                         </div>
                     </div>
+
+                    <div class="card col-xl-4 col-lg-6 col-12 ">
+                        <div class="card-block">
+                            <div class="card-header">
+                                Stewardship Fund Contribution
+                            </div>
+                            <div class="card-body">
+                                <fieldset class="form-group">
+                                    <small class="text-muted">Stewardship Fund Contribution</small>
+                                    <input type="number" step="1" class="form-control" id="other-income">
+                                </fieldset>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+
+
+
 
                 <div class="row match-height gx-3 col-12 w-100">
                     <button class="btn btn-primary px-2 py-1 mx-auto" type="submit">Submit</button>
