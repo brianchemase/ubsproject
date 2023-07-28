2d9a0b8fef6e3af6e4ec43cc0ac4ba0340244123
makambi-elvis <makambielvis002@gmail.com>
2023-07-28 12:48:11 +0300

    added dropdown menus

--- resources/views/users/inc/sidebar.blade.php
+++ resources/views/users/inc/sidebar.blade.php
@@ -14,20 +14,41 @@
             <li class="active"><a href="{{ route('dash') }}"><i class="ft-home"></i><span class="menu-title"
                         data-i18n="">Dashboard</span></a>
             </li>
-            <li class=" nav-item"><a href="{{ route('financialSustainabilityIndex') }}"><i class="ft-pie-chart"></i><span class="menu-title"
-                        data-i18n="">Finance Reports</span></a>
+            <li class=" nav-item has-sub"><a href="{{ route('financialSustainabilityIndex') }}"><i
+                        class="ft-pie-chart"></i><span class="menu-title" data-i18n="">Finance Reports</span></a>
+                <ul class="menu-content">
+                    <li><a class="menu-item" href="{{ route('incomeDiversificationForm') }}">Income Diversification</a>
+                    </li>
+                    <li><a class="menu-item" href="{{ route('financeManagementForm') }}">Finance Management</a>
+                    </li>
+                </ul>
             </li>
-            <li class=" nav-item"><a href="{{ route('missionSustainabilityIndex') }}"><i class="ft-droplet"></i><span class="menu-title"
-                        data-i18n="">Mission Reports</span></a>
+            <li class="nav-item has-sub"><a href="{{ route('missionSustainabilityIndex') }}"><i
+                        class="ft-layers"></i><span class="menu-title" data-i18n="">Mission Reports</span></a>
+                <ul class="menu-content">
+                    <li><a class="menu-item" href="{{ route('bibleTranslationForm') }}">Bible Translation</a>
+                    </li>
+                    <li><a class="menu-item" href="{{ route('biblePublishingAndDistributionForm') }}">Bible
+                            Distribution</a>
+                    </li>
+                    <li><a class="menu-item" href="{{ route('scriptureEngagementForm') }}">Scripture Engagement</a>
+                    </li>
+                </ul>
             </li>
             <li class=" nav-item"><a href="#"><i class="ft-layers"></i><span class="menu-title"
                         data-i18n="">Programme Reports</span></a>
             </li>
-            <li class=" nav-item"><a href="{{ route('collaborationsIndex') }}"><i class="ft-box"></i><span class="menu-title"
-                        data-i18n="">Collaborations</span></a>
+            <li class=" nav-item has-sub"><a href="{{ route('collaborationsIndex') }}"><i class="ft-box"></i><span
+                        class="menu-title" data-i18n="">Collaborations</span></a>
+                        <ul class="menu-content">
+                            <li><a class="menu-item" href="{{ route('membershipAndPartnership') }}">Membership and Partnership</a>
+                            </li>
+                            <li><a class="menu-item" href="{{ route('publicityAndCommunication') }}">Publicity and communication</a>
+                            </li>
+                        </ul>
             </li>
-            <li class=" nav-item"><a href="{{ route('userblankpage') }}"><i class="ft-bold"></i><span class="menu-title"
-                        data-i18n="">Blank Page</span></a>
+            <li class=" nav-item"><a href="{{ route('userblankpage') }}"><i class="ft-bold"></i><span
+                        class="menu-title" data-i18n="">Blank Page</span></a>
             </li>
             <li class=" nav-item"><a href="{{ route('usertablepage') }}"><i class="ft-credit-card"></i><span
                         class="menu-title" data-i18n="">Tables</span></a>
